6c8f83f8708fed2f4f06940c1e2b5f35ef5cd79b
Chris Hume <chris.hume@nxp.com>
2018-09-27 15:54:36 -0500

    Added IP-XACT 1685-2009 schema example (export).

--- lib/cross_origen/ip_xact.rb
+++ lib/cross_origen/ip_xact.rb
@@ -117,6 +117,13 @@ xsi:schemaLocation="$REGMEM_HOME/builder/ipxact/schema/ipxact
     end
 
     # Returns a string representing the owner object in IP-XACT XML
+    # Usable / Available options:
+    #   :vendor             = Company name/web address, ex: 'nxp.com'
+    #   :library            = IP Library
+    #   :schema             = '1685-2009' or default of Spirit 1.4 (when no :schema option passed)
+    #   :bus_interface      = only 'AMBA3' supported at this time
+    #   :mmap_name          = Optionally set the memoryMap name to something other than the module name
+    #   :mmap_ref           = memoryMapRef name, ex: 'UserMap'
     def owner_to_xml(options = {})
       require 'nokogiri'
 
@@ -124,23 +131,77 @@ xsi:schemaLocation="$REGMEM_HOME/builder/ipxact/schema/ipxact
         include_bit_field_values: true
       }.merge(options)
 
+      # Modified access type hash used in 1685-2009 schema
+      access_hash = {
+        ro:    { write: nil,             read: nil },
+        rw:    { write: nil,             read: nil },
+        rc:    { write: nil,             read: 'clear' },
+        rs:    { write: nil,             read: 'set' },
+        wrc:   { write: nil,             read: 'clear' },
+        wrs:   { write: nil,             read: 'set' },
+        wc:    { write: 'clear',         read: nil },
+        ws:    { write: 'set',           read: nil },
+        wsrc:  { write: 'set',           read: 'clear' },
+        wcrs:  { write: 'clear',         read: 'set' },
+        w1c:   { write: 'oneToClear',    read: nil },
+        w1s:   { write: 'oneToSet',      read: nil },
+        w1t:   { write: 'oneToToggle',   read: nil },
+        w0c:   { write: 'zeroToClear',   read: nil },
+        w0s:   { write: 'zeroToSet',     read: nil },
+        w0t:   { write: 'zeroToToggle',  read: nil },
+        w1src: { write: 'oneToSet',      read: 'clear' },
+        w1crs: { write: 'oneToClear',    read: 'set' },
+        w0src: { write: 'zeroToSet',     read: 'clear' },
+        w0crs: { write: 'zeroToClear',   read: 'set' },
+        wo:    { write: nil,             read: nil },
+        woc:   { write: 'clear',         read: nil },
+        worz:  { write: nil,             read: nil },
+        wos:   { write: 'set',           read: nil },
+        w1:    { write: nil,             read: nil },
+        wo1:   { write: nil,             read: nil },
+        dc:    { write: nil,             read: nil },
+        rowz:  { write: nil,             read: 'clear' }
+      }
+
       @format = options[:format]
 
-      schemas = [
-        'http://www.spiritconsortium.org/XMLSchema/SPIRIT/1.4',
-        'http://www.spiritconsortium.org/XMLSchema/SPIRIT/1.4/index.xsd'
-      ]
-      if uvm?
+      # Compatible schemas: Spirit 1.4, 1685-2009
+      # Assume Spirit 1.4 if no schema provided
+      if options[:schema] == '1685-2009' # Magillem tool uses alternate schema
+        schemas = [
+          'http://www.spiritconsortium.org/XMLSchema/SPIRIT/1685-2009',
+          'http://www.spiritconsortium.org/XMLSchema/SPIRIT/1685-2009/index.xsd'
+        ]
+      else # Assume Spirit 1.4 if not
+        schemas = [
+          'http://www.spiritconsortium.org/XMLSchema/SPIRIT/1.4',
+          'http://www.spiritconsortium.org/XMLSchema/SPIRIT/1.4/index.xsd'
+        ]
+      end
+
+      if uvm? && !(options[:schema] == '1685-2009')
         schemas << '$IREG_GEN/XMLSchema/SPIRIT/VendorExtensions.xsd'
       end
 
-      headers = {
-        'xmlns:spirit'       => 'http://www.spiritconsortium.org/XMLSchema/SPIRIT/1.4',
-        'xmlns:xsi'          => 'http://www.w3.org/2001/XMLSchema-instance',
-        'xsi:schemaLocation' => schemas.join(' ')
-      }
-      if uvm?
+      if options[:schema] == '1685-2009' # Magillem tool uses alternate schema
+        headers = {
+          'xmlns:spirit'       => 'http://www.spiritconsortium.org/XMLSchema/SPIRIT/1685-2009',
+          'xmlns:xsi'          => 'http://www.w3.org/2001/XMLSchema-instance',
+          'xsi:schemaLocation' => schemas.join(' ')
+        }
+      else # Assume Spirit 1.4 if not
+        headers = {
+          'xmlns:spirit'       => 'http://www.spiritconsortium.org/XMLSchema/SPIRIT/1.4',
+          'xmlns:xsi'          => 'http://www.w3.org/2001/XMLSchema-instance',
+          'xsi:schemaLocation' => schemas.join(' ')
+        }
+      end
+
+      if uvm? && !(options[:schema] == '1685-2009')
         headers['xmlns:vendorExtensions'] = '$IREG_GEN/XMLSchema/SPIRIT'
+        # Else:
+        # Do nothing ?
+        # headers['xmlns:vendorExtensions'] = '$UVM_RGM_HOME/builder/ipxact/schema'
       end
 
       builder = Nokogiri::XML::Builder.new(encoding: 'UTF-8') do |xml|
@@ -151,19 +212,38 @@ xsi:schemaLocation="$REGMEM_HOME/builder/ipxact/schema/ipxact
           # I guess this should really be the register owner's owner's name?
           spirit.name try(:ip_name) || owner.class.to_s.split('::').last
           spirit.version try(:ip_version, :version, :revision)
+          # The 1685-2009 schema allows for a bus interface.  AMBA3 (slave) supported so far.
+          if options[:schema] == '1685-2009'
+            if options[:bus_interface] == 'AMBA3'
+              spirit.busInterfaces do
+                spirit.busInterface do
+                  spirit.name 'Slave'
+                  bustype_header = {
+                    'spirit:vendor'  => options[:vendor] || 'Origen',
+                    'spirit:library' => 'amba3',
+                    'spirit:name'    => 'APB3',
+                    'spirit:version' => '1.0'
+                  }
+                  xml['spirit'].busType bustype_header
+                  spirit.slave do
+                    mmapref_header = {
+                      'spirit:memoryMapRef' => options[:mmap_ref] || 'APB'
+                    }
+                    xml['spirit'].memoryMapRef mmapref_header
+                  end
+                end
+              end
+            end
+          end
           spirit.memoryMaps do
             memory_maps.each do |map_name, _map|
               spirit.memoryMap do
-                spirit.name map_name
+                # Optionally assign memory map name to something other than the module name in Ruby
+                spirit.name options[:mmap_name] || map_name
                 address_blocks do |domain_name, _domain, sub_block|
                   spirit.addressBlock do
-                    if sub_block == owner
-                      spirit.name nil
-                      spirit.baseAddress 0.to_hex
-                    else
-                      spirit.name address_block_name(domain_name, sub_block)
-                      spirit.baseAddress sub_block.base_address.to_hex
-                    end
+                    spirit.name address_block_name(domain_name, sub_block)
+                    spirit.baseAddress sub_block.base_address.to_hex
                     spirit.range range(sub_block)
                     spirit.width width(sub_block)
                     sub_block.regs.each do |name, reg|
@@ -189,7 +269,42 @@ xsi:schemaLocation="$REGMEM_HOME/builder/ipxact/schema/ipxact
                             spirit.description try(bits, :brief_description, :name_full, :full_name)
                             spirit.bitOffset bits.position
                             spirit.bitWidth bits.size
-                            spirit.access bits.access
+                            if options[:schema] == '1685-2009' # Magillem tool uses alternate schema
+                              if bits.writable? && bits.readable?
+                                spirit.access 'read-write'
+                              elsif bits.writable?
+                                spirit.access 'write-only'
+                              elsif bits.readable?
+                                spirit.access 'read-only'
+                              end
+                              if bits.readable?
+                                unless access_hash[bits.access][:read].nil?
+                                  spirit.readAction access_hash[bits.access][:read]
+                                end
+                              end
+                              if bits.writable?
+                                unless access_hash[bits.access][:write].nil?
+                                  spirit.modifiedWriteValue access_hash[bits.access][:write]
+                                end
+                              end
+                            else # Assume Spirit 1.4 if not
+                              spirit.access bits.access
+                            end
+                            if options[:schema] == '1685-2009'
+                              spirit.parameters do
+                                spirit.parameter do
+                                  spirit.name '_hdlPath_'
+                                  spirit.value "ISYNTH.#{name.downcase}"
+                                end
+                              end
+                            end
+                            if options[:schema] == '1685-2009'
+                              spirit.vendorExtensions do
+                                vendorext = { 'xmlns:vendorExtensions' => '$UVM_RGM_HOME/builder/ipxact/schema' }
+                                xml['vendorExtensions'].hdl_path vendorext, "ISYNTH.#{name}"
+                              end
+                            end
+                            # spirit.access bits.access
                             if options[:include_bit_field_values]
                               if bits.bit_value_descriptions[0]
                                 bits.bit_value_descriptions.each do |val, desc|
@@ -201,7 +316,7 @@ xsi:schemaLocation="$REGMEM_HOME/builder/ipxact/schema/ipxact
                                 end
                               end
                             end
-                            if uvm?
+                            if uvm? && !(options[:schema] == '1685-2009')
                               spirit.vendorExtensions do
                                 xml['vendorExtensions'].hdl_path bits.path(relative_to: sub_block)
                               end
@@ -210,11 +325,12 @@ xsi:schemaLocation="$REGMEM_HOME/builder/ipxact/schema/ipxact
                         end
                       end
                     end
-                    if uvm?
-                      spirit.vendorExtensions do
-                        xml['vendorExtensions'].hdl_path sub_block.path(relative_to: owner)
-                      end
-                    end
+                    # Unclear whether addressBlock vendor extensions are supported in Spirit 1.4
+                    # if uvm?
+                    #  spirit.vendorExtensions do
+                    #    xml['vendorExtensions'].hdl_path sub_block.path(relative_to: owner)
+                    #  end
+                    # end
                   end
                 end
               end
@@ -271,7 +387,10 @@ xsi:schemaLocation="$REGMEM_HOME/builder/ipxact/schema/ipxact
     end
 
     def memory_maps
-      { nil => {} }
+      # C. Hume - This is non-functional for iregGen when the memory map name is empty -
+      # replacing with owner.name
+      # { nil => {} }
+      { owner.name => {} }
     end
 
     def sub_blocks(domain_name)
